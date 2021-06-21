6e5aaf372a92c0b4b0716dce69431a89fb99859c
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2021-06-21 15:26:57 -0400

    Update test-check_prefs_consistency.R

--- tests/testthat/test-check_prefs_consistency.R
+++ tests/testthat/test-check_prefs_consistency.R
@@ -1,16 +1,33 @@
 test_that("check_prefs_consistency() works", {
+  # expect note about bad pref names
   expect_error(
     check_prefs_consistency(
       list(not_a_pref = TRUE, not_a_pref2 = TRUE, not_a_pref3 = TRUE)),
     NA
   )
 
+  # expect note about bad value
   expect_error(
     check_prefs_consistency(
       list(rainbow_parentheses = "TRUE")),
     NA
   )
 
+  # expect note about bad value
+  expect_error(
+    check_prefs_consistency(
+      list(ansi_console_mode = TRUE)),
+    NA
+  )
+
+  # expect note about bad value
+  expect_error(
+    check_prefs_consistency(
+      list(font_size_points = "hello")),
+    NA
+  )
+
+  # expect NO NOTES
   expect_error(
     check_prefs_consistency(
       list(rainbow_parentheses = TRUE)),
