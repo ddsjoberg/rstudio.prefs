7213629e4bdfde3d83c09d97db69f22cfc414a2f
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2021-06-21 15:10:29 -0400

    Update test-check_prefs_consistency.R

--- tests/testthat/test-check_prefs_consistency.R
+++ tests/testthat/test-check_prefs_consistency.R
@@ -2,18 +2,18 @@ test_that("check_prefs_consistency() works", {
   expect_error(
     check_prefs_consistency(
       list(not_a_pref = TRUE, not_a_pref2 = TRUE, not_a_pref3 = TRUE)),
-    NULL
+    NA
   )
 
   expect_error(
     check_prefs_consistency(
       list(rainbow_parentheses = "TRUE")),
-    NULL
+    NA
   )
 
   expect_error(
     check_prefs_consistency(
       list(rainbow_parentheses = TRUE)),
-    NULL
+    NA
   )
 })
